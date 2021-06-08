fb351be538ecb2b711f1c635dabce2902b830fdc
poppo <poppo@woowahan.com>
2021-06-08 21:42:53 +0900

    project init

---
+++ src/main/java/com/example/springbatch/minela/SpringBatchByMinelaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbatch.minela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchByMinelaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchByMinelaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbatch/minela/SpringBatchByMinelaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbatch.minela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchByMinelaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
