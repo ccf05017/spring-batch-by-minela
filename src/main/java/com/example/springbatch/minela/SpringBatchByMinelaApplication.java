3881688be761cb5406ce265d7750f988606f5b72
poppo <poppo@woowahan.com>
2021-06-09 09:27:22 +0900

    temp

--- src/main/java/com/example/springbatch/minela/SpringBatchByMinelaApplication.java
+++ src/main/java/com/example/springbatch/minela/SpringBatchByMinelaApplication.java
@@ -1,10 +1,40 @@
 package com.example.springbatch.minela;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@EnableBatchProcessing
 @SpringBootApplication
 public class SpringBatchByMinelaApplication {
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("job")
+                .start(step())
+                .build();
+    }
+
+    @Bean
+    public Step step() {
+        return stepBuilderFactory.get("step")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("hello batch");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBatchByMinelaApplication.class, args);
---
+++ src/test/java/com/example/springbatch/minela/learningtests/TaskletTest.java
@@ -0,0 +1,10 @@
+package com.example.springbatch.minela.learningtests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+
+public class TaskletTest {
+    @Test
+    void simpleTest() {
+    }
+}
